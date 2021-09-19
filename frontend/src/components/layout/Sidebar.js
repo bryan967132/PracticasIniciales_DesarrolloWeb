75a460e6dcc06f42615a28a2134dddf4c3229c73
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2021-09-18 20:30:07 -0600

    back/front

--- frontend/src/components/layout/Sidebar.js
+++ frontend/src/components/layout/Sidebar.js
@@ -10,10 +10,11 @@ const Sidebar = () => {
             <div className="proyectos">
                 <h2>Publicaciones</h2>
                 <div className="clasificacion">
-                    <input type="radio" id="huey" name="drone" value="huey"/>Huey
-                    <input type="radio" id="huey" name="drone" value="huey"/>Huey
-                    <input type="radio" id="huey" name="drone" value="huey"/>Huey
-                    <input type="radio" id="huey" name="drone" value="huey"/>Huey
+                    <input type="radio" id="huey" name="drone" value="huey"/><a>Orden de publicación</a>
+                    <input type="radio" id="huey" name="drone" value="huey"/><a>Por Curso</a>
+                    <input type="radio" id="huey" name="drone" value="huey"/><a>Por Catedrático</a>
+                    <input type="radio" id="huey" name="drone" value="huey"/><a>Por Curso Específico</a>
+                    <input type="radio" id="huey" name="drone" value="huey"/><a>Por Catedrático</a>
                 </div>
             </div>
         </aside>
