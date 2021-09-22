b7cc3b7aa4829674ccd54511583398f17129148e
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2021-09-21 23:06:57 -0600

    Publicacion Form

--- frontend/src/components/publicaciones/FormPublicacion.js
+++ frontend/src/components/publicaciones/FormPublicacion.js
@@ -4,15 +4,30 @@ const FormTarea = () => {
     return(
         <div className="formulario">
             <form>
-                <div className="contenedor-input">
+                <div className="cuerpo contenedor-input">
                     <input
                         type="text"
                         className="input-text"
                         placeholder="Publicación..."
+                        name="contenido"
+                    />
+                </div>
+                <div className="opc comboBox">
+                    <select className="opciones" name="optionlist" onChange="combo(this, 'demo')">
+                        <option>Curso</option>
+                        <option>Catedrático</option>
+                    </select>
+                </div>
+
+                <div className="cur-cat contenedor-input">
+                    <input
+                        type="text"
+                        className="input-text"
+                        placeholder="Curso o Catedrático..."
                         name="nombre"
                     />
                 </div>
-                <div className="contenedor-input">
+                <div className="pub contenedor-input">
                     <input
                         type="submit"
                         className="btn btn-primario btn-submit btn-block"
