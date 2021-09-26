709bece9722e0658dbdc4ecc786bb90c67784c6c
bryan967132 <81400332+bryan967132@users.noreply.github.com>
2021-09-26 09:39:57 -0600

    layout

--- frontend/src/components/layout/Sidebar4.js
+++ frontend/src/components/layout/Sidebar4.js
@@ -2,33 +2,33 @@ import React from "react";
 import BuscarUsuario from "../inicio/BuscarUsuario";
 import {Link} from 'react-router-dom';
 
-const Sidebar4 = () => {
+const Sidebar4 = ({carnet}) => {
     return (
         <aside>
             <h1>DTT</h1>
             <BuscarUsuario/>
             <div className="proyectos">
-                <Link to={'/inicio'} className="enlace-publicacion">Publicaciones</Link>
+                <Link to={`inicio?carnet=${carnet}`} className="enlace-publicacion">Publicaciones</Link>
                 <h3>Clasificar por:</h3>
                 <div className="clasificacion">
-                    <Link to={'/inicio'}>
+                    <Link to={`inicio?carnet=${carnet}`}>
                         <input type="radio" id="ord" name="drone" value="ord"/>
                     </Link><a>Orden de publicación</a>
 
-                    <Link to={'/publicaciones-por-curso'}>
+                    <Link to={`/publicaciones-por-curso?carnet=${carnet}`}>
                         <input type="radio" id="cur" name="drone" value="cur"/>
                     </Link><a>Por Curso</a>
 
-                    <Link to={'/publicaciones-por-catedratico'}>
+                    <Link to={`/publicaciones-por-catedratico?carnet=${carnet}`}>
                         <input type="radio" id="cat" name="drone" value="cat"/>
                     </Link><a>Por Catedrático</a>
 
-                    <Link to={'/publicaciones-por-curso-especifico'}>
+                    <Link to={`/publicaciones-por-curso-especifico?carnet=${carnet}`}>
                         <input type="radio" id="cures" name="drone" value="cures"/>
                     </Link><a>Por Curso Específico</a>
 
-                    <Link to={'/publicaciones-por-catedratico-especifico'}>
-                        <input type="radio" id="cates" name="drone" value="cates" defaultChecked/>
+                    <Link to={`/publicaciones-por-catedratico-especifico?carnet=${carnet}`}>
+                        <input type="radio" id="cates" name="drone" value="cates" checked/>
                     </Link><a>Por Catedrático Específico</a>
                 </div>
             </div>
