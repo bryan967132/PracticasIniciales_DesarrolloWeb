e4ab2056884ec8c27967d2175049c2b44d9c068b
bryan967132 <81400332+bryan967132@users.noreply.github.com>
2021-09-26 09:31:24 -0600

    Perfil

--- frontend/src/components/inicio/MiPerfil.js
+++ frontend/src/components/inicio/MiPerfil.js
@@ -11,16 +11,37 @@ const MiPerfil = () => {
     headers.append('Access-Control-Allow-Origin','http://localhost:4000');
     headers.append('Access-Control-Allow-Credentials','true');
     headers.append('GET','POST','OPTIONS','PUT','DELETE');
+
+    function Remove(str, startIndex, count) {
+        return str.substr(0, startIndex) + str.substr(startIndex + count);
+    }
+
+    let url = Object.values(window.location);
+    let json = JSON.stringify(url);
+    let arr = Remove(json,0,4);
+    let valor = Remove(arr,arr.length-1,arr.length);
+    let sep = valor.split(',');
+    let cadena = sep[0]
+    let arr1 = Remove(cadena,0,1);
+    let valor1 = Remove(arr1,arr1.length-1,arr1.length);
+    let carnet = Remove(valor1,0,39);
+
     return (
         <div className="contenedor-app">
-            <Sidebar/>
+            <Sidebar
+                carnet={carnet}
+            />
             <div className="seccion-principal">
-                <Barra/>
+                <Barra
+                    carnet={carnet}
+                />
                 <main className="contenedor-perfil">
                     <div className="contenedor-cursos">
                         <CursosAprobados/>
                     </div>
-                    <Informacion/>
+                    <Informacion
+                        carnet={carnet}
+                    />
                 </main>
             </div>
         </div>
